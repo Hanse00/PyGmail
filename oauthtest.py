ef58ad349ec43d81269af9a9fc58caf4d2247d52
Philip Hansen <me@philiphansen.dk>
2015-01-12 23:20:08 +0100

    Print access token once it has been gotten

--- oauthtest.py
+++ oauthtest.py
@@ -1,8 +1,13 @@
 import json
 import requests
+import time
+import sys
+import logging
 
 POST_URL = "https://accounts.google.com/o/oauth2/device/code"
+TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
 SCOPE = "profile"
+GRANT_TYPE = "http://oauth.net/grant_type/device/1.0"
 
 f = open("client_secret.json", "r")
 client_secret_file = json.load(f)
@@ -14,8 +19,9 @@ client_secret = client_secret_file['installed']['client_secret']
 print "Client ID: " + client_id
 print "Client Secret: " + client_secret
 
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 payload = {'client_id': client_id, "scope": SCOPE}
-response = requests.post(POST_URL, params=payload)
+response = requests.post(POST_URL, data=payload, headers=headers)
 
 response_json = json.loads(response.text)
 
@@ -24,3 +30,23 @@ user_code = response_json['user_code']
 
 print "Verification URL: " + verification_url
 print "User code: " + user_code
+
+poll_time = 0
+sleep = response_json['interval']
+expiration_window = response_json['expires_in']
+device_code = response_json['device_code']
+
+payload2 = {'client_id': client_id, 'client_secret': client_secret, 'code': device_code, 'grant_type': GRANT_TYPE}
+while poll_time <= expiration_window:
+    print "Wait time: " + str(poll_time) + " seconds."
+    response2 = requests.post(TOKEN_URL, data=payload2, headers=headers)
+    response_json2 = json.loads(response2.text)
+    if (not "error" in response_json2):
+        break
+    print json.loads(response2.text)
+    poll_time += sleep
+    time.sleep(sleep)
+else:
+    print "Login timed out"
+    sys.exit(0)
+print json.loads(response2.text)
