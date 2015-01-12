3fb5e9eab6a3f253df874d3de45243ed0d034b90
Philip Hansen <me@philiphansen.dk>
2015-01-12 22:04:04 +0100

    Get google API response based on client id and secret

--- oauthtest.py
+++ oauthtest.py
@@ -1,8 +1,18 @@
 import json
+from subprocess import call
+
+POST_URL = "https://accounts.google.com/o/oauth2/device/code"
+SCOPE = "profile"
 
 f = open("client_secret.json", "r")
-client_secret = json.load(f)
+client_secret_file = json.load(f)
 f.close()
 
-print "Client ID: " + client_secret['installed']['client_id']
-print "Client Secret: " + client_secret['installed']['client_secret']
+client_id =client_secret_file['installed']['client_id']
+client_secret = client_secret_file['installed']['client_secret']
+
+print "Client ID: " + client_id
+print "Client Secret: " + client_secret
+
+response = call("curl -d \"client_id=" + client_id + "&scope=" + SCOPE + "\" " + POST_URL)
+print response
